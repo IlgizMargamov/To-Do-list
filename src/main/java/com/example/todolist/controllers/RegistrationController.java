3d1557166e9fd007dda6be949dd65519e9b20868
RomanNechaev <romenkagg@gmail.com>
2022-11-02 22:34:14 +0500

    change redirect

--- src/main/java/com/example/todolist/controllers/RegistrationController.java
+++ src/main/java/com/example/todolist/controllers/RegistrationController.java
@@ -17,41 +17,40 @@ import java.util.List;
 
 @Controller
 public class RegistrationController {
-   private final UserService m_userService;
+    private final UserService m_userService;
     private final TaskServiceFactory m_taskServiceFactory;
 
-   @Inject
-   public RegistrationController(UserService userService, TaskServiceFactory taskServiceFactory){
-       m_userService=userService;
-       m_taskServiceFactory = taskServiceFactory;
-   }
+    @Inject
+    public RegistrationController(UserService userService, TaskServiceFactory taskServiceFactory) {
+        m_userService = userService;
+        m_taskServiceFactory = taskServiceFactory;
+    }
 
-   @GetMapping("/getUser")
-   @ResponseBody
-   public List<Task> getUser(){
-       AbstractTaskService abstractTaskService = m_taskServiceFactory.getService();
-       return abstractTaskService.getTasks();
-   }
+    @GetMapping("/getUser")
+    @ResponseBody
+    public List<Task> getUser() {
+        AbstractTaskService abstractTaskService = m_taskServiceFactory.getService();
+        return abstractTaskService.getTasks();
+    }
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public String index() {
+        return "redirect:/tasks";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Model model){
-       try {
-           m_userService.addUser(user);
-           return "redirect:/login";
-       }
-       catch (Exception ex){
-           model.addAttribute("message", "User already exists");
-           return "registration";
-       }
+    public String addUser(User user, Model model) {
+        try {
+            m_userService.addUser(user);
+            return "redirect:/login";
+        } catch (Exception ex) {
+            model.addAttribute("message", "User already exists");
+            return "registration";
+        }
     }
 }
